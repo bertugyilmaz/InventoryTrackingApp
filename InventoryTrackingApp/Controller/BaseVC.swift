a7e2758ea16570b4676d18fd71865d83da2d3441
Bertuğ <ylmazbertug@gmail.com>
2017-12-09 00:10:53 +0300

    minor changes

---
+++ InventoryTrackingApp/Controller/BaseVC.swift
@@ -0,0 +1,24 @@
+//
+//  BaseVC.swift
+//  InventoryTrackingApp
+//
+//  Created by Bertuğ YILMAZ on 09/12/2017.
+//  Copyright © 2017 Bertuğ YILMAZ. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+
+
+}
--- InventoryTrackingApp/Controller/LoginVC.swift
+++ InventoryTrackingApp/Controller/LoginVC.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class LoginVC : UIViewController {
+class LoginVC : BaseVC {
     @IBOutlet weak var userName : UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
