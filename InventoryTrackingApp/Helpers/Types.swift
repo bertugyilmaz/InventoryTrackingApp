102065472647d9bea7ea9b62aabfc8b88c4358ef
Onur Hüseyin Çantay <ohc3807@gmail.com>
2017-12-11 00:34:39 +0300

    Creating Models and connecting Firebase With MainVC #8

---
+++ InventoryTrackingApp/Helpers/Types.swift
@@ -0,0 +1,9 @@
+//
+//  Types.swift
+//  InventoryTrackingApp
+//
+//  Created by onur hüseyin çantay on 10.12.2017.
+//  Copyright © 2017 Bertuğ YILMAZ. All rights reserved.
+//
+
+import Foundation
